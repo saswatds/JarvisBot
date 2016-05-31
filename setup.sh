a3d75c7de8b71673182e00f21e6f1f3caca948d4
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2016-05-31 13:35:15 +0100

    Stop annoying "Do you want to continue prompt"

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 baselocation=$PWD
 apt-get update
-apt-get install libasound2-dev memcached python-pip mpg123 python-alsaaudio python-aubio
+apt-get -y install libasound2-dev memcached python-pip mpg123 python-alsaaudio python-aubio
 pip install -r requirements.txt
 cp initd_alexa.sh /etc/init.d/alexa
 cd /etc/rc5.d

3aef32ce5d569aad31eb723f88034c0d2676422b
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2016-05-31 15:52:36 +0100

    Add comments to example

--- example_creds.py
+++ example_creds.py
@@ -1,3 +1,7 @@
+# This is just an EXAMPLE file. You do not need to look at this unless you are having difficulties
+# A creds.py file should be automatically created by setup.sh when run and the user fills in their details from Amazon
+# To remove the creds.py (and its compiled companion) currently in place, run reset_alexa.sh
+
 # RENAME THIS FILE TO creds.py before use!
 import os
 

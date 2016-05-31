68faa88d2d421f35e281002193e983ad0a4e4fd8
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2016-05-31 14:45:31 +0000

    Add a reset file

---
+++ reset_alexa.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Script to remove the credentials from this directory, therefore resetting Alexa
+# If you want to change the Amazon Alexa Voice Service device you are using, please re-run setup.sh (sudo ./setup.sh) and fill out the necessary NEW credentials, as if setting up from scratch
+# Created by Matthew Timmons-Brown, The Raspberry Pi Guy
+
+rm creds.py, creds.pyc
